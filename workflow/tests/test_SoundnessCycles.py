3d3df75bc16b7f1c59d71cc5726060881434d42b
IndikaKuma <ipkumarawd@yahoo.com>
2020-10-26 12:36:03 +0100

    Fix path location

--- workflow/tests/test_SoundnessCycles.py
+++ workflow/tests/test_SoundnessCycles.py
@@ -6,7 +6,7 @@ from pnmlpy import WorkflowVerifier
 class TestAdminByDefault:
 
     def test_file(self):
-        errors = WorkflowVerifier.run_verifier('../../testResources/runningexample.pnml')
+        errors = WorkflowVerifier.run_verifier('testResources/runningexample.pnml')
         import json
         js = json.loads(json.dumps(errors, sort_keys=False, indent=4))
         assert True == js["soundness"]
