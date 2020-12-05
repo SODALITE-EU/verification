27ced116449a8ba969b5bb0be21f88805b4381e5
IndikaKuma <ipkumarawd@yahoo.com>
2020-12-05 22:09:55 +0100

    Fixed error due updated APIs

--- workflow/pnmlpy/WorkflowVerifier.py
+++ workflow/pnmlpy/WorkflowVerifier.py
@@ -1,12 +1,12 @@
 import os
 
-from pm4py.objects.petri import check_soundness
+from pm4py.objects.petri.check_soundness import check_easy_soundness_net_in_fin_marking
 from pm4py.objects.petri import utils
-from pm4py.objects.petri.importer import pnml as pnml_importer
+from pm4py.objects.petri.importer import importer as petri_importer
 
 
 def run_verifier(file):
-    net, initial_marking, final_marking = pnml_importer.import_net(file)
+    net, initial_marking, final_marking = petri_importer.apply(file)
     cycles = utils.get_cycles_petri_net_places(net)
-    soundness = check_soundness.check_petri_wfnet_and_soundness(net)
+    soundness = check_easy_soundness_net_in_fin_marking(net, initial_marking, final_marking)
     return {"soundness": soundness, "cycles": len(cycles)}
