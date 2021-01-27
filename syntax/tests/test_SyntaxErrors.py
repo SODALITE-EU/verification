309e29de61ea3b1559ec653b961ec4bb1aa04e50
IndikaKuma <ipkumarawd@yahoo.com>
2021-01-27 15:21:50 +0100

    Fixed an smell reported by sonar

--- syntax/tests/test_SyntaxErrors.py
+++ syntax/tests/test_SyntaxErrors.py
@@ -1,7 +1,7 @@
 from tosca import TOSCASyntaxVerifier
 
 
-class TestAdminByDefault:
+class TestSyntaxChecks:
 
     def test_file(self):
         errors = TOSCASyntaxVerifier.run_verifier('testResources/tosca_single_instance_wordpress.yaml')
@@ -9,5 +9,10 @@ class TestAdminByDefault:
         import json
         js = json.loads(json.dumps(errors, sort_keys=False, indent=4))
         js2 = json.loads(json.dumps(errors2, sort_keys=False, indent=4))
-        assert 'ValidationError' == js["error_type"]
+        error_type = js["error_type"]
+        assert 'ValidationError' == error_type
+        assert 'MissingRequiredFieldError' != error_type
+        assert 'UnknownFieldError' != error_type
+        assert 'InvalidTemplateVersion' != error_type
+        assert 'TOSCAException' != error_type
         assert 0 == len(js2)
