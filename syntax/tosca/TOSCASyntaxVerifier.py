3651b2440a920d8fcce96209d8b8e808bda4ac2f
IndikaKuma <ipkumarawd@yahoo.com>
2021-01-27 15:48:58 +0100

    Fixed an smell reported by sonar

--- syntax/tosca/TOSCASyntaxVerifier.py
+++ syntax/tosca/TOSCASyntaxVerifier.py
@@ -1,6 +1,5 @@
 from toscaparser import shell as parser_shell
-from toscaparser.common.exception import TOSCAException, ValidationError, UnknownFieldError, MissingRequiredFieldError, \
-    InvalidTemplateVersion
+from toscaparser.common.exception import TOSCAException
 
 
 def run_verifier(file):
@@ -8,24 +7,6 @@ def run_verifier(file):
     bug_data = ""
     try:
         parser_shell.ParserShell().parse(file)
-    except ValueError as value_error:
-        bug_data = str(value_error)
-        bug_type = type(value_error)
-    except AttributeError as att_error:
-        bug_data = str(att_error)
-        bug_type = type(att_error)
-    except ValidationError as val_error:
-        bug_data = str(val_error.message)
-        bug_type = type(val_error)
-    except UnknownFieldError as uf_error:
-        bug_data = str(uf_error.message)
-        bug_type = type(uf_error)
-    except MissingRequiredFieldError as mrfe_error:
-        bug_data = str(mrfe_error.message)
-        bug_type = type(mrfe_error)
-    except InvalidTemplateVersion as itv_error:
-        bug_data = str(itv_error.message)
-        bug_type = type(itv_error)
     except TOSCAException as general_error:
         bug_type = type(general_error)
         bug_data = general_error.message
